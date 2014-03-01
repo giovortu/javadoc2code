9ba73bd2a12c801605ecdd0f8d439f3ef93808f7
Mark Saunders <mscoolnerd@gmail.com>
2014-03-01 17:23:02 -0500

    This fixes #2 and #3 by making the method generation code a little more intelligent.

--- javadoc2code/src/javadoc2code/wizards/JavadocConverter.java
+++ javadoc2code/src/javadoc2code/wizards/JavadocConverter.java
@@ -14,48 +14,46 @@ import org.jsoup.select.Elements;
 
 /**
  * @author Mark
- *
+ * 
  */
 public class JavadocConverter {
 	final static int LINE_LENGTH = 60;
-	
-	public static String convertJavadoc(String url) throws IOException
-	{	
+
+	public static String convertJavadoc(String url) throws IOException {
 		Document doc = Jsoup.connect(url).get();
 		doc = Jsoup.parse(doc.html().replace("&nbsp;", " "));
 		doc.outputSettings().escapeMode(Entities.EscapeMode.xhtml);
-	
-		//FileWriter outputFile = new FileWriter(filename);
+
+		// FileWriter outputFile = new FileWriter(filename);
 		StringBuilder outputFile = new StringBuilder();
-		
+
 		// Write classname
 		String classDeclaration = getClassDeclaration(doc);
 		outputFile.append(classDeclaration);
 		outputFile.append(" {\n\n");
-		
+
 		boolean isInterface = classDeclaration.contains(" interface ");
-		
-		if(!isInterface){
+
+		if (!isInterface) {
 			// Write constructor
-			outputFile.append(getMethods(doc, "constructor_detail", isInterface));
+			outputFile
+					.append(getMethods(doc, "constructor_detail", isInterface));
 		}
-		
-		// Write methods 
+
+		// Write methods
 		outputFile.append(getMethods(doc, "method_detail", isInterface));
-		
+
 		outputFile.append("}\n");
 		return outputFile.toString();
 	}
-	
-	private static String getClassDeclaration(Document htmlDoc)
-	{	
+
+	private static String getClassDeclaration(Document htmlDoc) {
 		StringBuilder declarationString = new StringBuilder();
 		Element classDeclaration = htmlDoc.select("pre").get(0);
 		declarationString.append("/**\n");
-		String[] classDescription = Utils.breakupString(
-				classDeclaration.siblingElements().select("div").text(), LINE_LENGTH);
-		for (String s : classDescription)
-		{
+		String[] classDescription = Utils.breakupString(classDeclaration
+				.siblingElements().select("div").text(), LINE_LENGTH);
+		for (String s : classDescription) {
 			declarationString.append(" * " + s + "\n");
 		}
 		declarationString.append(" *\n");
@@ -64,33 +62,56 @@ public class JavadocConverter {
 		declarationString.append(classDeclaration.text().replace("\n", " "));
 		return declarationString.toString();
 	}
-	
-	private static String getMethods(Document htmlDoc, String category, boolean isInterface)
-	{
+
+	private static String getMethods(Document htmlDoc, String category,
+			boolean isInterface) {
 		StringBuilder methodString = new StringBuilder();
-		Elements categoryElements =  htmlDoc.select("a[name=" + category + "]");
-		
-		if (!categoryElements.isEmpty()){
+		Elements categoryElements = htmlDoc.select("a[name=" + category + "]");
+
+		if (!categoryElements.isEmpty()) {
 			Element methodDetail = categoryElements.first();
 			Elements methods = methodDetail.siblingElements().select("li");
-			
-			for (Element method : methods)
-			{
+
+			for (Element method : methods) {
 				methodString.append("\t/**\n");
-				String[] methodDescription = Utils.breakupString(method.select("div").text(), LINE_LENGTH);
-				for (String s : methodDescription){
-					methodString.append("\t * " + s  + "\n");
+				
+				String[] methodDescription = Utils.breakupString(
+						method.select("div").text(), LINE_LENGTH);
+				
+				// Grab the descriptionS of the method
+				for (String s : methodDescription) {
+					methodString.append("\t * " + s + "\n");
 				}
-				for (Element param : method.select("dl dd"))
-				{	
-					if (param.text().contains("-") && (param.childNodeSize() > 0)){
+				
+				boolean containsThrows = false;
+				StringBuilder throwsString = new StringBuilder();
+				// Get all the parameters and throws
+				for (Element param : method.select("dl dd")) {
+					if (param.text().matches("\\w+\\s-\\s.*")
+							&& (param.childNodeSize() > 0)) {
+						// Check to see if we have entered the throws section
+						if(!containsThrows)
+						{
+							if (param.previousElementSibling().text().equals("Throws:"))
+							{
+								containsThrows = true;
+							}
+						}
 						String[] paramInfo = param.text().split("-", 2);
-						methodString.append("\t * @param \t" 
-								+ paramInfo[0].trim() + "\t " 
+						if (!containsThrows){
+							methodString.append("\t * @param \t");
+						} else {
+							methodString.append("\t * @throws \t");
+							// Add the throws types to their own string for adding
+							// to the method declaration
+							throwsString.append(paramInfo[0].trim() + " ");
+						}
+						methodString.append(paramInfo[0].trim() + "\t "
 								+ paramInfo[1].trim() + "\n");
 					} else {
-						if(param.previousElementSibling().text().equals("Returns:")){
-							methodString.append("\t * @returns \t" 
+						if (param.previousElementSibling().text()
+								.equals("Returns:")) {
+							methodString.append("\t * @returns \t"
 									+ param.text().trim() + "\n");
 						}
 					}
@@ -98,7 +119,11 @@ public class JavadocConverter {
 				methodString.append("\t */\n");
 				String methodDeclaration = method.select("pre").text();
 				methodString.append("\t" + methodDeclaration);
-				if (methodDeclaration.contains(" abstract ") || isInterface){
+				if (containsThrows)
+				{
+					methodString.append(" throws " + throwsString);
+				}
+				if (methodDeclaration.contains(" abstract ") || isInterface) {
 					methodString.append(";\n\n");
 				} else {
 					methodString.append(" {\n");
